b8c89f198c1f967832b42e74890e51f147ca2ef3
sudheerdarla <sudheer.darla11@gmail.com>
2018-02-23 12:29:19 +0530

    update 2048

--- ch11/2048.py
+++ ch11/2048.py
@@ -1,6 +1,6 @@
 #! python3
-# 2048.py will go to the 2048 game website and play the
-## automatically with up,right,left and down arrows given by selenium module
+# 2048.py will go to the 2048 game website and play the automatically with 
+## up,right,left and down arrows given by selenium Keys module
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,13 +9,6 @@ import bs4, requests, time, random
 browser = webdriver.Firefox()
 browser.get('https://gabrielecirulli.github.io/2048/')
 htmlElem = browser.find_element_by_tag_name('html')
-
-res = requests.get('https://gabrielecirulli.github.io/2048/')
-res.raise_for_status()
-
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-gOver = soup.select('.game-over')
 
 randKey = [Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT]
 
